2b9bfa5a79ec8813d728a5cb50a9cfbf7ce72db3
darwindu(杜志星) <darwindu@webank.com>
2019-01-09 10:36:40 +0800

    sdk

--- src/main/java/com/webank/weid/http/protocol/request/ReqUpdateCptArgs.java
+++ src/main/java/com/webank/weid/http/protocol/request/ReqUpdateCptArgs.java
@@ -1,5 +1,7 @@
 package com.webank.weid.http.protocol.request;
 
+import java.util.LinkedHashMap;
+
 import lombok.Data;
 
 @Data
@@ -13,7 +15,7 @@ public class ReqUpdateCptArgs {
     /**
      * Required: the json schema content defined for this CPT.
      */
-    private String cptJsonSchema;
+    private LinkedHashMap<String, Object> cptJsonSchema;
 
     /**
      * Required: the WeIdentity DID of the publisher who register this CPT.
