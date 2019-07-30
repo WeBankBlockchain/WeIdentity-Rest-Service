dd557557b9e838184b625944ed22bd44e81a9e40
chaoxinhu <chaoxinhu@hotmail.com>
2019-07-30 15:43:22 +0800

    - Add controller, service and impl classes for endpoint service - Add basic storage unit for endpoint info - Add UT and new error codes - Fix several configuration bugs - Add RpcClient manager (i.e. ServerConnector) - Add endpoint merge feature

---
+++ src/main/java/com/webank/weid/http/service/rpc/RpcClient.java
@@ -0,0 +1,166 @@
+/*
+ *       Copyright© (2019) WeBank Co., Ltd.
+ *
+ *       This file is part of weidentity-http-service.
+ *
+ *       weidentity-http-service is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       weidentity-http-service is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with weidentity-http-service.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.webank.weid.http.service.rpc;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smartboot.socket.MessageProcessor;
+import org.smartboot.socket.StateMachineEnum;
+import org.smartboot.socket.transport.AioQuickClient;
+import org.smartboot.socket.transport.AioSession;
+
+import com.webank.weid.http.constant.HttpReturnCode;
+import com.webank.weid.http.constant.WeIdentityServiceEndpoint;
+import com.webank.weid.http.protocol.response.HttpResponseData;
+import com.webank.weid.http.util.PropertiesUtil;
+
+
+public class RpcClient {
+    private static final Logger logger = LoggerFactory.getLogger(RpcClient.class);
+
+    private static final int STATE_PENDING = 0;
+    private static final int STATE_RECEIVED = 1;
+    private static final int STATE_TIMEOUT = 2;
+    private static final int STATE_FAILED = 3;
+    private static final int POLL_INTERNAL_MILLIS = 500;
+    private static final int MAX_RETRIES = 20;
+
+    private static final Integer DEFAULT_LISTENER_PORT =
+        Integer.valueOf(PropertiesUtil.getProperty("default.listener.port"));
+
+    // The results and states per uuid w.r.t session.
+    private AioSession<String> session = null;
+    private Map<String, String> resultMap = new ConcurrentHashMap<>();
+    private Map<String, Integer> stateMap = new ConcurrentHashMap<>();
+
+    // The host/port information for this client.
+    @Getter
+    @Setter
+    private String host;
+    @Getter
+    @Setter
+    private int port;
+
+    /**
+     * Constructor with supplied host and port String.
+     */
+    protected RpcClient(String hostport) throws Exception {
+        if (hostport.contains(":")) {
+            setHostPort(hostport);
+        } else {
+            this.host = hostport;
+            this.port = DEFAULT_LISTENER_PORT;
+        }
+        session = getNewSession(this.host, this.port);
+    }
+
+    /**
+     * Enforce a client re-connect based on pre-set host and port.
+     */
+    protected void reconnect() throws Exception {
+        if (isValid()) {
+            session.close();
+        }
+        session = getNewSession(host, port);
+    }
+
+    /**
+     * Return the validity of this client.
+     */
+    protected boolean isValid() {
+        return (session != null) && !session.isInvalid();
+    }
+
+    /**
+     * Send the message to RPC server.
+     */
+    protected HttpResponseData<String> send(String msg) {
+        String uuid = UUID.randomUUID().toString();
+        String message = msg + WeIdentityServiceEndpoint.EPS_SEPARATOR + uuid;
+        byte[] msgBody = message.getBytes();
+        byte[] msgHead = {(byte) msgBody.length};
+        resultMap.put(uuid, StringUtils.EMPTY);
+        stateMap.put(uuid, STATE_PENDING);
+        try {
+            session.writeBuffer().write(msgHead);
+            session.writeBuffer().write(msgBody);
+            session.writeBuffer().flush();
+        } catch (IOException e) {
+            stateMap.put(uuid, STATE_FAILED);
+            logger.error("Failed to send message. ", e.getMessage());
+            return new HttpResponseData<>(StringUtils.EMPTY,
+                HttpReturnCode.RPC_SEND_FAIL.getCode(), e.getMessage());
+        }
+        return new HttpResponseData<>(uuid, HttpReturnCode.SUCCESS);
+    }
+
+    /**
+     * Periodically fetch the result from the given UUID.
+     */
+    protected HttpResponseData<String> get(String uuid) throws Exception {
+        for (int i = 0; i < MAX_RETRIES; i++) {
+            if (stateMap.get(uuid) == STATE_RECEIVED) {
+                return new HttpResponseData<>(resultMap.get(uuid), HttpReturnCode.SUCCESS);
+            }
+            Thread.sleep(POLL_INTERNAL_MILLIS);
+        }
+        stateMap.put(uuid, STATE_TIMEOUT);
+        return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.RPC_FETCH_FAIL);
+    }
+
+    /**
+     * Close the RPC connection. Can be re-opened via the reconnect() function.
+     */
+    protected void close() {
+        session.close();
+    }
+
+    private AioSession<String> getNewSession(String host, int port)
+        throws InterruptedException, ExecutionException, IOException {
+        AioQuickClient<String> client = new AioQuickClient<String>(host, port,
+            new StringProtocol(), new MessageProcessor<String>() {
+            public void process(AioSession<String> session, String msg) {
+                String uuid = msg.substring(msg.length() - 36);
+                resultMap.put(uuid, msg.substring(0, msg.length() - 39));
+                stateMap.put(uuid, STATE_RECEIVED);
+            }
+
+            public void stateEvent(AioSession<String> session, StateMachineEnum stateMachineEnum,
+                Throwable throwable) {
+            }
+        });
+
+        return client.start();
+    }
+
+    protected void setHostPort(String hostPort) {
+        this.host = StringUtils.splitByWholeSeparator(hostPort, ":")[0];
+        this.port = Integer.valueOf(StringUtils.splitByWholeSeparator(hostPort, ":")[1]);
+    }
+}
---
+++ src/main/java/com/webank/weid/http/service/rpc/RpcConnectionHandler.java
@@ -0,0 +1,212 @@
+/*
+ *       Copyright© (2019) WeBank Co., Ltd.
+ *
+ *       This file is part of weidentity-http-service.
+ *
+ *       weidentity-http-service is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       weidentity-http-service is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with weidentity-http-service.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.webank.weid.http.service.rpc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.webank.weid.http.constant.HttpReturnCode;
+import com.webank.weid.http.constant.WeIdentityServiceEndpoint;
+import com.webank.weid.http.protocol.request.EndpointRequest;
+import com.webank.weid.http.protocol.response.EndpointInfo;
+import com.webank.weid.http.protocol.response.HttpResponseData;
+import com.webank.weid.http.util.EndpointDataUtil;
+import com.webank.weid.http.util.PropertiesUtil;
+import com.webank.weid.util.DataToolUtils;
+
+/**
+ * Client side handler, build remote rpc server connection and simple rpc client management, e.g.
+ * create/close, multi-active, and auto-clean. Provides simple interfaces for external call.
+ */
+public class RpcConnectionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(RpcConnectionHandler.class);
+
+    // Mapping key: request (UUID), value: a remote host/port
+    private Map<String, String> uuidHostMap = new ConcurrentHashMap<>();
+
+    // Mapping key: hostport, value: one active RpcClient
+    private Map<String, RpcClient> hostClientsMap = new ConcurrentHashMap<>();
+
+    // Singleton
+    private volatile static RpcConnectionHandler rpcConnectionHandler;
+
+    private RpcConnectionHandler() {
+    }
+
+    public static RpcConnectionHandler getInstance() {
+        if (rpcConnectionHandler == null) {
+            synchronized (RpcConnectionHandler.class) {
+                if (rpcConnectionHandler == null) {
+                    rpcConnectionHandler = new RpcConnectionHandler();
+                }
+            }
+        }
+        return rpcConnectionHandler;
+
+    }
+
+    // Background process to periodically fetch remote's endpoint info per defined seconds.
+    {
+        Runnable runnable = new Runnable() {
+            public void run() {
+                List<String> inAddrList = Arrays
+                    .asList(PropertiesUtil.getProperty("server.hostport.list").split(","));
+                fetchAndMergeEndpoints(inAddrList);
+            }
+        };
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(runnable, 10, 60, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Invoke a one-time RPC send based on given address and request and get the UUID reply.
+     *
+     * @param hostport address to be sent to
+     * @param endpointRequest the endpoint request
+     * @return String reply message
+     */
+    public HttpResponseData<String> send(String hostport, EndpointRequest endpointRequest) {
+        RpcClient rpcClient;
+        try {
+            if (hostClientsMap.containsKey(hostport)) {
+                rpcClient = hostClientsMap.get(hostport);
+                if (!rpcClient.isValid()) {
+                    rpcClient.reconnect();
+                }
+            } else {
+                rpcClient = new RpcClient(hostport);
+                hostClientsMap.put(hostport, rpcClient);
+            }
+            String uuid = rpcClient
+                .send(endpointRequest.getRequestName() + WeIdentityServiceEndpoint.EPS_SEPARATOR
+                    + endpointRequest.getRequestBody())
+                .getRespBody();
+            if (!StringUtils.isEmpty(uuid)) {
+                uuidHostMap.put(uuid, hostport);
+                return new HttpResponseData<>(uuid, HttpReturnCode.SUCCESS);
+            } else {
+                logger.info("Cannot obtain a valid session UUID when sending to: ", hostport);
+                return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.RPC_GET_FAIL);
+            }
+        } catch (Exception e) {
+            logger.error("Failed to send to remote server: ", hostport);
+            return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.RPC_SEND_FAIL);
+        }
+    }
+
+    /**
+     * Invoke an RPC random send based on given Endpoint info and request and get the UUID reply.
+     * This is a random Send procedure - sender will randomly pick an address in the list and send
+     * out the request. If it fails, then send to next VM until succeeded.
+     *
+     * @param inAddrList destination address list
+     * @return UUID index
+     */
+    public HttpResponseData<String> randomSend(List<String> inAddrList,
+        EndpointRequest endpointRequest) {
+        double gap = (double) 1 / inAddrList.size();
+        double pick = Math.random();
+        for (int i = 0; i < inAddrList.size(); i++) {
+            // check whether to send in this round
+            if (pick < (((double) (i + 1)) * gap)) {
+                HttpResponseData<String> sendReply = send(inAddrList.get(i), endpointRequest);
+                if (StringUtils.isEmpty(sendReply.getRespBody())) {
+                    pick += gap;
+                    logger.error("A RandomSend attempt failed: " + inAddrList.get(i));
+                } else {
+                    return sendReply;
+                }
+            }
+        }
+        return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.RPC_SEND_FAIL);
+    }
+
+    /**
+     * Async get the result from remote's reply.
+     *
+     * @param uuid the uuid
+     * @return result
+     */
+    public HttpResponseData<String> get(String uuid) {
+        String hostPort = uuidHostMap.get(uuid);
+        if (StringUtils.isEmpty(hostPort)) {
+            return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.INPUT_ILLEGAL);
+        }
+        RpcClient rpcClient = hostClientsMap.get(hostPort);
+        if (rpcClient == null) {
+            return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.RPC_NETWORK_ERROR);
+        }
+        try {
+            return rpcClient.get(uuid);
+        } catch (Exception e) {
+            logger.error("Failed to get this session UUID info: " + uuid);
+            return new HttpResponseData<>(StringUtils.EMPTY, HttpReturnCode.RPC_GET_FAIL);
+        }
+    }
+
+    /**
+     * Fetch remote all server's endpoints in a best-effort manner, and merge to local data store.
+     *
+     * @param addrList remote address list
+     */
+    private void fetchAndMergeEndpoints(List<String> addrList) {
+        logger.info("Now: " + System.currentTimeMillis() + " Starting to poll remote service at: "
+            + EndpointDataUtil.stringListToString(addrList));
+        System.out.println("Now: " + System.currentTimeMillis() + " Starting to poll remote service"
+            + " at: " + EndpointDataUtil.stringListToString(addrList));
+        EndpointRequest endpointRequest = new EndpointRequest();
+        endpointRequest.setRequestName(WeIdentityServiceEndpoint.FETCH_FUNCTION);
+        EndpointDataUtil.clearProps();
+        for (String hostport : addrList) {
+            try {
+                String uuid = send(hostport, endpointRequest).getRespBody();
+                if (!StringUtils.isEmpty(uuid)) {
+                    String endpointInfoString = get(uuid).getRespBody();
+                    EndpointInfo endpointInfo = DataToolUtils
+                        .deserialize(endpointInfoString, EndpointInfo.class);
+                    List<String> newList = new ArrayList<>();
+                    newList.add(hostport);
+                    endpointInfo.setInAddr(newList);
+                    EndpointDataUtil.mergeToCentral(endpointInfo);
+                } else {
+                    logger.info("Failed to connect to remote host (reply null): " + hostport);
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+        try {
+            EndpointDataUtil.saveEndpointsToFile();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
