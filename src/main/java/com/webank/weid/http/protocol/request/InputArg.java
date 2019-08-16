70944864a803f99b22fde4737d37ea20657cc47c
chaoxinhu <chaoxinhu@hotmail.com>
2019-08-16 17:29:47 +0800

    - Revise Copyright headers

--- src/main/java/com/webank/weid/http/protocol/request/InputArg.java
+++ src/main/java/com/webank/weid/http/protocol/request/InputArg.java
@@ -1,20 +1,20 @@
 /*
  *       Copyright© (2019) WeBank Co., Ltd.
  *
- *       This file is part of weidentity-http-service.
+ *       This file is part of weid-http-service.
  *
- *       weidentity-http-service is free software: you can redistribute it and/or modify
+ *       weid-http-service is free software: you can redistribute it and/or modify
  *       it under the terms of the GNU Lesser General Public License as published by
  *       the Free Software Foundation, either version 3 of the License, or
  *       (at your option) any later version.
  *
- *       weidentity-http-service is distributed in the hope that it will be useful,
+ *       weid-http-service is distributed in the hope that it will be useful,
  *       but WITHOUT ANY WARRANTY; without even the implied warranty of
  *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *       GNU Lesser General Public License for more details.
  *
  *       You should have received a copy of the GNU Lesser General Public License
- *       along with weidentity-http-service.  If not, see <https://www.gnu.org/licenses/>.
+ *       along with weid-http-service.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.webank.weid.http.protocol.request;
