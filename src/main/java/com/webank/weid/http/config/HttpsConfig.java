091a24eaebf906ab1ac3f225be2abe87bee9d146
chaoxinhu <chaoxinhu@hotmail.com>
2020-03-13 20:09:08 +0800

    * Enable HTTPS with self-signed cert and ciphers

--- src/main/java/com/webank/weid/http/config/HttpsConfig.java
+++ src/main/java/com/webank/weid/http/config/HttpsConfig.java
@@ -19,13 +19,11 @@
 
 package com.webank.weid.http.config;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,46 +31,32 @@ import org.springframework.context.annotation.Configuration;
 /**
  * implement http redirect https.
  *
- * @author darwindu
+ * @author darwindu/chaoxinhu
  */
 @Configuration
 @ConditionalOnProperty(value = "server.ssl.enabled", havingValue = "true")
 public class HttpsConfig {
 
-    @Value("${server.port}")
-    private Integer port;
-
     @Value("${server.http.port}")
-    private Integer httpPort;
+    private Integer serverPort;
 
+    /**
+     * support HTTP request processing.
+     */
     @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
             @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    TomcatEmbeddedServletContainerFactory containerFactory =
+                        (TomcatEmbeddedServletContainerFactory) container;
+                    Connector connector =
+                        new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+                    connector.setPort(serverPort);
+                    containerFactory.addAdditionalTomcatConnectors(connector);
+                }
             }
         };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
     }
-
-
-    @Bean
-    public Connector httpConnector() {
-
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(httpPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(port);
-        return connector;
-    }
-
 }
